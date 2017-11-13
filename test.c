1c3ec01d1f227cb178ac4d9f095031760b4f5217
zuojxin <zuojxin@hotmail.com>
2017-11-13 14:07:30 +0800

    ii

--- test.c
+++ test.c
@@ -52,7 +52,7 @@ static void test_parse_null()
     lept_value v;
     v.type = LEPT_FALSE;
     
-    EXPECT_EQ_INT(LEPT_PARSE_ROOT_NOT_SINGULAR ,lept_parse(&v, "null"));
+    EXPECT_EQ_INT(LEPT_PARSE_OK ,lept_parse(&v, "null"));
     EXPECT_EQ_INT(LEPT_NULL , lept_get_type(&v));
 }
 
@@ -61,7 +61,7 @@ static void test_parse_false()
     lept_value v;
     v.type = LEPT_FALSE;
     
-    EXPECT_EQ_INT(LEPT_FALSE ,lept_parse(&v, "false"));
+    EXPECT_EQ_INT(LEPT_PARSE_OK ,lept_parse(&v, "false"));
     EXPECT_EQ_INT(LEPT_FALSE , lept_get_type(&v));
 }
 
@@ -70,7 +70,7 @@ static void test_parse_true()
     lept_value v;
     v.type = LEPT_FALSE;
     
-    EXPECT_EQ_INT(LEPT_TRUE ,lept_parse(&v, "true"));
+    EXPECT_EQ_INT(LEPT_PARSE_OK ,lept_parse(&v, "true"));
     EXPECT_EQ_INT(LEPT_TRUE , lept_get_type(&v));
 }
 static void test_parse()
